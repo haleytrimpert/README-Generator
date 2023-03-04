a16a3bbb37ec987ae7e4aa2f3cedc88d0b18c0b5
Haley T <hntrimpert@yahoo.com>
2023-03-03 19:56:45 -0500

    added questions array

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title?",
+ "What is the description?", 
+ "What are the installation instructions?",
+"What is the usage information?",
+"What are the contribution guidelines?",
+"What are the test instructions?",
+"What is your github username?",
+"What is your email?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
